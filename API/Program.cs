47dbd45fd2a98785ebafb74558df85faed22aae1
ONUR DERMAN <onrdrmn@gmail.com>
2023-04-19 03:05:41 +0300

    #2 - Frontend General Structure Created with Required Settings

--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .AllowAnyHeader()
+            .WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +34,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
