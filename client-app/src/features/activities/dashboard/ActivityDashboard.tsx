f23e4275cc955dc16aa142ac01a5bcb7323943c3
ONUR DERMAN <onrdrmn@gmail.com>
2023-04-20 00:35:29 +0300

    #4 - User Interface for CRUD using React

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
+
+interface Props {
+  activities: Activity[];
+  selectedActivity: Activity | undefined;
+  editMode: boolean;
+  selectActivity: (id: string) => void;
+  cancelSelectActivity: () => void;
+  openForm: (id: string) => void;
+  closeForm: () => void;
+  createOrEdit: (activity: Activity) => void;
+  deleteActivity: (id: string) => void;
+}
+
+export default function ActivityDashboard({
+  activities,
+  selectedActivity,
+  editMode,
+  selectActivity,
+  cancelSelectActivity,
+  openForm,
+  closeForm,
+  createOrEdit,
+  deleteActivity
+}: Props) {
+  return (
+    <Grid>
+      <Grid.Column width="10">
+        <ActivityList 
+          activities={activities} 
+          selectActivity={selectActivity}  
+          deleteActivity={deleteActivity}
+          />
+      </Grid.Column>
+      <Grid.Column width="6">
+        {selectedActivity && !editMode &&
+        <ActivityDetails 
+          activity={selectedActivity}   
+          cancelSelectActivity={cancelSelectActivity} 
+          openForm={openForm}
+          />}
+          {editMode &&
+        <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+      </Grid.Column>
+    </Grid>
+  );
+}
